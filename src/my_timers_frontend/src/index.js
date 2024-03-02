9093201fc340aa0de0a81f62332520f7fb406ba3
luxeave <luxeaves@gmail.com>
2024-03-02 20:05:42 +0700

    Initial commit.

---
+++ src/my_timers_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { my_timers_backend } from "../../declarations/my_timers_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await my_timers_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
